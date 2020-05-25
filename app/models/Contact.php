088186a248d9a312f54c5688b53389bda2827d28
lawalthb <lawalthb@gmail.com>
2020-05-25 21:55:44 +0100

    Contact CRUD completed

---
+++ app/models/Contact.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'city',
+        'country',
+        'job_title'       
+    ];
+}
